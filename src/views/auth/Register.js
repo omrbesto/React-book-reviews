a055bf0ca2e4d059c6d01136f846ffb7f035ce76
nitiponth <ntitiponth@outlook.com>
2021-04-13 14:05:09 +0700

    add twitter and Facebook button

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -21,9 +21,20 @@ export default function Register() {
                     <img
                       alt="..."
                       className="w-5 mr-1"
-                      src={require("assets/img/github.svg").default}
+                      src={require("assets/img/facebook.svg").default}
                     />
-                    Github
+                    facebook
+                  </button>
+                  <button
+                    className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    <img
+                      alt="..."
+                      className="w-5 mr-1"
+                      src={require("assets/img/twitter.svg").default}
+                    />
+                    twitter
                   </button>
                   <button
                     className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
